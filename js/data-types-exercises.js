32d654a774414e33e03b5a20c5017537069150cf
kleflo <flakeskylan@gmail.com>
2022-08-01 04:45:22 -0500

    update on exercises

---
+++ js/data-types-exercises.js
@@ -0,0 +1,29 @@
+Identify the data type of the following values:
+1. 99.99
+// number
+2. "false"
+// string
+3. false
+// boolean
+4. '0'
+// string
+5. 0
+// number
+6. true
+// boolean
+7. 'true'
+// string
+
+What data type would best represent:
+1. a term or phrase typed into a search box
+// string
+2. if a user is logged in
+// boolean
+3. a discount amount to apply to a user's shopping cart
+// number
+4. whether or not a coupon code is valid
+// boolean
+5. an email address typed into a registration form
+// string
+6. the price of a product
+// number
---
+++ js/operators-exercises.js
@@ -0,0 +1,39 @@
+1. '1' + 2
+// 12
+2. typeof '1' + 2
+// string2
+3. 6 % 4
+// 2
+4. '3 + 4 is ' + 3 + 4
+// how could you make this produce the correct output?
+// '3 + 4' !== 3 + 4
+// side note I am not sure this is correct.
+5. 0 < 0
+// false
+6. 'false' == false
+// true
+// it is false because '==' is equal in value, they ARE NOT equal in value..but how are they not equal in value?
+7. true == 'true'
+// false
+8. 5 >= -5
+// true
+9. !false || false
+// true
+10. true || "42"
+// true
+11. !true && !false
+// false
+12. 6 % 5
+// 1
+13. 5 < 4 && 1 ===
+// false
+14. typeof 'codeup' === 'string'
+// true
+15. 'codeup' === 'codeup' && 'codeup' === 'Codeup'
+// false , is it false because of the capital C? 
+16. 4 >= 0 && 1 !== '1'
+// true
+17. 6 % 3 === 0
+// true
+18. 5 % 2 !== 0
+// true
