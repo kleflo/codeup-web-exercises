6b256b77667c5ee3915dbdfebafba334bd39dbc4
kleflo <flakeskylan@gmail.com>
2022-08-01 08:11:56 -0500

    updates to external js

--- js/external.js
+++ js/external.js
@@ -2,13 +2,13 @@ alert('Welcome to my Website!');
 console.log("Hello from external JavaScript");
 let colorFavorite = prompt('What is your favorite color?');
 alert(colorFavorite + ' is my favorite color too! ');
-/*
-* A student can be enrolled in a class only if the class is not full and the class schedule does not conflict with her current schedule.
-* A product offer can be applied only if a person buys more than 2 items, and the offer has not expired. Premium members do not need to buy a specific amount of products.*/
+/* A product offer can be applied only if a person buys more than 2 items, and the offer has not expired. Premium members do not need to buy a specific amount of products.*/
 let movRent = prompt('You have rented some movies for your kids: The little mermaid (for 3 days), Brother Bear (for 5 days), + and Hercules (1 day). If price for a movie per day is $3, how much will you have to pay?');
 alert(movRent = 27);
 let contr = prompt('Suppose you\'re working as a contractor for 3 companies: Google, Amazon and Facebook, they pay you a different rate per hour. Google pays $400, Amazon $380, and Facebook $350. How much will you receive in payment for this week? You worked 10 hours for Facebook, 6 hours for Google and 4 hours for Amazon.')
 // Google = 2400, Amazon = 1520 Facebook = 3500
 alert(contr = 7420);
-let collStudent = prompt('A student can be enrolled in a class only if the class is not full and the class schedule does not conflict with her current schedule.')
+let collStudent = alert('A student can be enrolled in a class only if the class is not full and the class schedule does not conflict with her current schedule.')
 console.log(collStudent = true);
+let prodOffer = alert('A product offer can be applied only if a person buys more than 2 items, and the offer has not expired. Premium members do not need to buy a specific amount of products.');
+
