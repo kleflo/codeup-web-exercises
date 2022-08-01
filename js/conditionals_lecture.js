4da4579243adf223615338d4db9d3794c31c4ccc
kleflo <flakeskylan@gmail.com>
2022-08-01 16:26:49 -0500

    functions and conditionals

---
+++ js/conditionals_lecture.js
@@ -0,0 +1,12 @@
+var username = prompt('enter your username').toLowerCase();
+var password = prompt('enter your password').toLowerCase();
+
+var currUsername = 'username'
+
+var currPassword = 'password'
+
+if(username === currUsername && password === currPassword) {
+    alert('you have logged in')
+}else{
+    
+}
